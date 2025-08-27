a28eb0fc30296458a97e72ab65d890c5cb22605b
matheusHaig <matheushaigz09@gmail.com>
2025-08-27 16:29:48 -0300

    cria a classe TaxCalculator

--- src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
+++ src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
@@ -1,10 +1,13 @@
 package br.com.mariojp.solid.srp;
 
 public class ReceiptService {
+
 	public String generate(Order order) {
 		double subtotal = order.getItems().stream().mapToDouble(i -> i.getUnitPrice() * i.getQuantity()).sum();
-		double tax = subtotal * 0.10; //Taxa 10 fixa :(
-		double total = subtotal + tax;
+        TaxCalculator tax = new TaxCalculator();
+        double taxa = tax.calculaTaxa(subtotal,0.10);
+		 //Taxa 10 fixa :(
+		double total = subtotal + taxa;
 		StringBuilder sb = new StringBuilder(); //Formatando o Recibo
 		sb.append("=== RECIBO ===\n");
 		for (var i : order.getItems()) {
